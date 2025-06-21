7246a725d527c7b1e7e6652feadf161b66c0d2b0
Fazal2307-dev <mdfazal2307@gmail.com>
2025-06-21 19:15:55 +0530

    adding new component

--- React-learning/src/App.jsx
+++ React-learning/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import Body from './Components/Body'
 
 const App = () => {
   return (
     <div>
     
       <Navbar />
+      <Body />
     </div>
   )
 }
---
+++ React-learning/src/Components/Body.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Body = () => {
+  return (
+    <div>
+      <h4>hey what r u doing</h4>
+    </div>
+  )
+}
+
+export default Body
