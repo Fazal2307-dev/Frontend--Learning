449e951170b7b352ffd597833db8098945a137f6
Fazal2307-dev <mdfazal2307@gmail.com>
2025-06-23 09:29:46 +0530

    ss

--- React-learning/src/Components/Card.jsx
+++ React-learning/src/Components/Card.jsx
@@ -6,8 +6,8 @@ function Card() {
       <div className='w-52 bg-zinc-100 absolute left-1/2 top-1/2 -translate-x-[50%] -translate-y-[50%] rounded-md overfollow-hidden'>
             <div className='w-full h-32 bg-zinc-300'></div>
             <div className='w-full px-3 py-4'>
-                <h2>Amazon Basic.</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti laborum nam, dolorem porro nesciunt ex dolores harum debitis, nulla adipisci voluptatem quia quam fuga tempora ullam temporibus! Ullam, dignissimos porro!</p>
+                <h2 className='font-semibold'>Amazon Basic.</h2>
+                <p className='text-xs'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             </div>
       </div>
     </div>
