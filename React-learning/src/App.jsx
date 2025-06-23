d2302ab94bd1ef8bf49ac50bd8d7b04786a0eabe
Fazal2307-dev <mdfazal2307@gmail.com>
2025-06-23 17:03:12 +0530

    dynamic data

--- React-learning/src/App.jsx
+++ React-learning/src/App.jsx
@@ -3,10 +3,17 @@ import React from 'react'
 import Card from './Components/Card'
 
 const App = () => {
+
+  const Array = ["Fazal","Afzal","Sanya","ALiya"];
+
   return (
     <div>
+     { Array.map((elem,index) =>(
+      <div className='w-fit px-3 py-4 bg-zinc-400'>
+        {elem}
+      </div>
+     ))}
     
-     <Card />
  
     </div>
   )
