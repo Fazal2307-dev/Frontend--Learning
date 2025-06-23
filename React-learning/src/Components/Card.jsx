adddee7638ad1041f190c8fbcde94e375c08ecc7
Fazal2307-dev <mdfazal2307@gmail.com>
2025-06-23 09:18:45 +0530

    updating card

--- React-learning/src/Components/Card.jsx
+++ React-learning/src/Components/Card.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Card() {
   return (
-    <div>
-      
+    <div className='w-full h-screen bg-zinc-500'>
+      <div className='px-3 py-4 bg-zinc-100 absolute left-1/2 top-1/2 -translate-x-[50%] -translate-y-[50%] rounded-md '></div>
     </div>
   )
 }
