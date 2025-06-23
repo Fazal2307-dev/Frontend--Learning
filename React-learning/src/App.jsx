62165333ea5462800abd92779dd4f5a984214b8f
Fazal2307-dev <mdfazal2307@gmail.com>
2025-06-23 09:04:30 +0530

    building card

--- React-learning/src/App.jsx
+++ React-learning/src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import Navbar from './Components/Navbar'
-import Body from './Components/Body'
+
+import Card from './Components/Card'
 
 const App = () => {
   return (
     <div>
     
-      <Navbar />
-      <Body />
+     <Card />
     </div>
   )
 }
