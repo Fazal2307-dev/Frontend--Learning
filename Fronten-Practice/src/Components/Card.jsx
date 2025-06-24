20d380514a061f98d39bbdff2eb303f0d9d21864
Fazal2307-dev <mdfazal2307@gmail.com>
2025-06-24 13:21:31 +0530

    build

--- Fronten-Practice/src/Components/Card.jsx
+++ Fronten-Practice/src/Components/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Card() {
   return (
     <div>
-      <h3>Lorem ipsum dolor sit amet.</h3>
+      <h3 className='bg-sky-400'>Lorem ipsum dolor sit amet.</h3>
     </div>
   )
 }
