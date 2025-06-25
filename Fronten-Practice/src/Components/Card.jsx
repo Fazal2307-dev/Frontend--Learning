d2527d547a990687ab1bc8f6192948ff2a45a70d
Fazal2307-dev <mdfazal2307@gmail.com>
2025-06-25 20:09:43 +0530

    usestate

--- Fronten-Practice/src/Components/Card.jsx
+++ Fronten-Practice/src/Components/Card.jsx
@@ -4,12 +4,12 @@ function Card() {
   return (
     <div className='w-full h-screen bg-blue-200 flex justify-center items-center  '>
       <div className='w-52 h-fit bg-red-200 rounded-md overflow-hidden '>
-        <div>
+        <div className=''>
           <img src="https://images.unsplash.com/photo-1750490970730-493c7ea3fdf2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D" alt="" />
         </div>
           <div>
-            <h2 className="text-xl m-2">Amazon.in</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis dolorum blanditiis vero nobis alias, optio, cum dolorem quas, sit saepe veritatis maiores sint? Quisquam ad aperiam nostrum. Nam quisquam repellendus, nobis voluptatum quibusdam commodi aliquid veniam. Culpa, molestias? Error optio quibusdam explicabo sequi accusamus minima officia temporibus rem beatae itaque?</p>
+            <h2 className="text-xl m-1">Amazon.in</h2>
+            <p className='m-1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis dolorum blanditiis vero nobis alias, optio, cum dolorem quas, sit saepe veritatis maiores sint? Quisquam ad aperiam nostrum.</p>
           </div>
       </div>
   
