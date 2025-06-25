e055f4c0198b107f80f1ce2d5671a894f4abb404
Fazal2307-dev <mdfazal2307@gmail.com>
2025-06-25 21:07:51 +0530

    changing dynamic val in

--- React-learning/src/App.jsx
+++ React-learning/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <div >
       <h2>{val.name}</h2>
       <h2>{val.isBanned .toString()}</h2>
-      <button   onClick={()=>setVal({...val, name:"Value change with the help of spred fn",isBanned:! val.isBanned})} className='bg-amber-300 rounded -full '>update value of object</button>
+      <button   onClick={()=>setVal({...val, name:"Value change with the help of spred fn",isBanned:! val.isBanned})} className={` rounded-full text-white p-2 ${val.isBanned ? "bg-blue-500" :"bg-red-500"}`} >update value of object</button>
     </div>
   )
 }
