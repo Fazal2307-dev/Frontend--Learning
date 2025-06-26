14cb3d4a6932ad1645eced7201eb713f87060470
Fazal2307-dev <mdfazal2307@gmail.com>
2025-06-26 18:33:48 +0530

    das

---
+++ Fronten-Practice/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <ol className=' flex justify-center items-center gap-10'>
+        <li>AMAZON.IN</li>
+        <li>Location</li>
+        <li>Search bar</li>
+        <li>Country</li>
+        <li>Account</li>
+        <li>Reaturn & Order</li>
+        <li>Cart</li>
+      </ol>
+   
+    </div>
+  )
+}
+
+export default Navbar
