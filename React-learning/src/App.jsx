0a32d8b0f4f96ee03ba8675a55ac21d0fbbde48e
Fazal2307-dev <mdfazal2307@gmail.com>
2025-06-29 18:21:32 +0530

    using logic to remove element

--- React-learning/src/App.jsx
+++ React-learning/src/App.jsx
@@ -1,14 +1,12 @@
 import React, { useState } from 'react'
 
 function App() {
- const[val,setVal]= useState([1,2,3,4,5,6])
+ const [val,setVal]= useState([1,2,3,4,5,6])
   return (
     <div className='p-5'>
-      <h1>
-      {val.map(item=><h1>{item}</h1>)}
-      </h1>
-      <button  onClick={()=>setVal(()=>val.filter((item,index) => index != val.length-1))}
-      className='text-white bg-blue-500 rounded-full'>Click me</button>
+      {val.map(item => <h1>{item}</h1>)}
+      <button onClick ={()=>setVal(()=> val.filter((item,index)=> index!= 3))}
+      className='text-white bg-blue-500 rounded-full m-2 '> Click me</button>
     </div>
   )
 }
