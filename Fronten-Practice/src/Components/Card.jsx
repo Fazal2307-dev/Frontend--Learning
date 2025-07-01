a8862a681f92c61255ec619ecfce75505fc47ed5
Fazal2307-dev <mdfazal2307@gmail.com>
2025-07-01 21:38:22 +0530

    asa

--- Fronten-Practice/src/Components/Card.jsx
+++ Fronten-Practice/src/Components/Card.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
 function Card() {
+  
   return (
     <div className='p-4'>
       <p>You Travlled {} This Km </p>
-      <button>Click me</button>
+      <button className='bg-blue-500 rounded m-4 text-amber-50'>Click me</button>
     </div>
   )
 }
