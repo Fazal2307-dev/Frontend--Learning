3f92a529d565890eea25d1983422edd49d1d6f9a
Fazal2307-dev <mdfazal2307@gmail.com>
2025-07-01 21:34:41 +0530

    ss

--- Fronten-Practice/src/App.jsx
+++ Fronten-Practice/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Card from './Components/Card'
-import Navbar from './Components/Navbar/Navbar'
+
 
 function App() {
   return (
     <div >
-      <Navbar />
+      
       <Card />
 
     </div>
