6ba6466be4f03ac9aa527830dc5a993f32823ecb
Fazal2307-dev <mdfazal2307@gmail.com>
2025-07-01 21:45:24 +0530

    aasa

--- Fronten-Practice/src/Components/Card.jsx
+++ Fronten-Practice/src/Components/Card.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Card() {
-  
+  const[val,setVal]=useState(0)
   return (
     <div className='p-4'>
-      <p>You Travlled {} This Km </p>
-      <button className='bg-blue-500 rounded m-4 text-amber-50'>Click me</button>
+      <p>You Travlled {val} This Km </p>
+      <button  onClick={()=>{setVal(()=> val+1)}}  className='bg-blue-500 rounded m-4 text-amber-50'>Click me</button>
     </div>
   )
 }
