73ae8b7ecfd70a538609c320695795750580bcc1
Fazal2307-dev <mdfazal2307@gmail.com>
2025-07-01 22:37:25 +0530

    aa

--- Fronten-Practice/src/Components/Card.jsx
+++ Fronten-Practice/src/Components/Card.jsx
@@ -4,8 +4,8 @@ function Card() {
   const[val,setVal]=useState(0)
   return (
     <div className='p-4'>
-      <p>You Travlled {val} This Km </p>
-      <button  onClick={()=>{setVal(()=> val+1)}}  className='bg-blue-500 rounded m-4 text-amber-50'>Click me</button>
+      <p>You Travlled {val} Km </p>
+      <button  onClick={()=>{setVal(()=> val+1)}}  className='bg-blue-500 rounded m-4 p-2 text-amber-50'>Click me</button>
     </div>
   )
 }
