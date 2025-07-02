ac651f6b168f93b0f2ab0377323ef3398568c691
Fazal2307-dev <mdfazal2307@gmail.com>
2025-07-02 18:30:38 +0530

    as

--- React-learning/src/App.jsx
+++ React-learning/src/App.jsx
@@ -1,12 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
+import Card from './Components/Card'
 
 function App() {
- const [val,setVal]= useState([1,2,3,4,5,6])
   return (
-    <div className='p-5'>
-      {val.map(item => <h1>{item}</h1>)}
-      <button onClick ={()=>setVal(()=> val.filter((item,index)=> index!= 3))}
-      className='text-white bg-blue-500 rounded-full m-2 '> Click me</button>
+    <div>
+      <Card />
     </div>
   )
 }
