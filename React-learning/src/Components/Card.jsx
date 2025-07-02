c9afdc31ad52242caae16ac4398fe9031909e775
Fazal2307-dev <mdfazal2307@gmail.com>
2025-07-02 18:42:03 +0530

    maping

--- React-learning/src/Components/Card.jsx
+++ React-learning/src/Components/Card.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function Card() {
+ const [val,setVal] =  useState([
+    {name:"Fazal",age:23},
+    {name:"Afzal",age:21},
+    {name:"Saniya",age:13}
+  ])
   return (
     <div>
-      
+      {val.map((item)=>
+      <div>
+         <h1>{item.name}</h1>
+         <h2>{item.age}</h2>
+      </div>
+      )}
     </div>
   )
 }
