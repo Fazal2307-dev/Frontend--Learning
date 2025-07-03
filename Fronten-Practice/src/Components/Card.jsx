fb67fa35d54ef52a922d01d41844a00380eee6da
Fazal2307-dev <mdfazal2307@gmail.com>
2025-07-03 10:11:58 +0530

    dsa

--- Fronten-Practice/src/Components/Card.jsx
+++ Fronten-Practice/src/Components/Card.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function Card() {
   return (
     <div >
-      <div className='w-52 h-fit bg-amber-200'>
+      <div className='w-52 h-fit bg-amber-200 rounded m-3  '>
         <img src="https://images.unsplash.com/photo-1747597197690-de29ae7d998e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyM3x8fGVufDB8fHx8fA%3D%3D" alt="" />
-        <h1>Amazon.in</h1>
+        <h1 className=' text-2xl'>Amazon.in</h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident, minima quam! Rem.</p>
       </div>
     </div>
